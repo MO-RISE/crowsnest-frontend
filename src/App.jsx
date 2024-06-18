cc7df04df554f5954058011fc16becc9eb45c103
Linnea Olsson <linnea.x.olsson@ri.se>
2024-06-18 11:56:08 +0200

    add route to alert list page

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ import Lookout360 from "./apps/e-lookout/subpage_360"
 import CameraStreams from "./apps/e-lookout/camera-streams"
 import ELookoutCamArray from "./apps/e-lookout/camera-array"
 import WindPower from "./apps/wind-power"
+import AlertList from "./apps/alert-list/AlertList"
 
 export default function App() {
   const app_state = useRecoilValue(appState)
@@ -56,16 +57,18 @@ export default function App() {
               <Route exact path={ROUTES.DATA_FLOW} element={<PageDataFlow />} />
               <Route exact path={ROUTES.CONFIGURATION} element={<PageOSConfig />} />
               <Route exact path={ROUTES.SETTINGS} element={<PageSettings />} />
-             
+
               <Route exact path={ROUTES.E_LOOKOUT} element={<ELookout />} />
               <Route exact path={ROUTES.CAMERA_STREAMS} element={<CameraStreams />} />
               <Route exact path={ROUTES.LOOKOUT_360} element={<Lookout360 />} />
               <Route exact path={ROUTES.CAMERA_ARRAY} element={<ELookoutCamArray />} />
-             
+
               <Route exact path={ROUTES.REMOTE_CONTROL} element={<PageRemoteControl />} />
               <Route exact path={ROUTES.ROUTE_EDITOR} element={<RouteEditor />} />
 
               <Route exact path={ROUTES.WIND_POWER} element={<WindPower />} />
+
+              <Route exact path={ROUTES.ALERT_LIST} element={<AlertList />} />
             </Routes>
           </BasePage>
         </Router>
